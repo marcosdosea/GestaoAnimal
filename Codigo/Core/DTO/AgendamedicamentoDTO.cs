eaa7f000bb22f0dfd5e021f2a8ab0c9b8de48c17
Gilson Caio <gilsoncaio7@gmail.com>
2021-02-01 20:53:08 -0300

    Revert "Feat/77 - Melhorias no caso de uso Agendar Medicamento" (#90)

--- Codigo/Core/DTO/AgendamedicamentoDTO.cs
+++ Codigo/Core/DTO/AgendamedicamentoDTO.cs
@@ -6,15 +6,10 @@ namespace Core
 {
     public class AgendamedicamentoDTO
     {
-        public int IdAgendamento { get; set; }
         public int? Frequencia { get; set; }
         public DateTime? DataInicio { get; set; }
         public DateTime? DataTermino { get; set; }
         public int? Intervalo { get; set; }
         public string Dosagem { get; set; }
-        public string NomePessoa { get; set; }
-        public string NomeAnimal { get; set; }
-        public string NomeMedicamento { get; set; }
-        public string NomeConsulta { get; set; }
     }
 }
--- Codigo/GestaoAnimalWeb/Controllers/AgendamedicamentoController.cs
+++ Codigo/GestaoAnimalWeb/Controllers/AgendamedicamentoController.cs
@@ -4,32 +4,17 @@ using Core;
 using AutoMapper;
 using Models;
 using System.Collections.Generic;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using System;
 
 namespace Controllers
 {
     public class AgendamedicamentoController : Controller
     {
         IAgendamedicamentoService _agendamedicamentoService;
-        IMedicamentoService _medicamentoService;
-        IAnimalService _animalService;
-        IPessoaService _pessoaService;
-        IConsultaService _consultaService;
         IMapper _mapper;
 
-        public AgendamedicamentoController(IAgendamedicamentoService agendamedicamentoService,
-            IMedicamentoService medicamentoService, 
-            IAnimalService animalService,
-            IConsultaService consultaService,
-            //IPessoaService pessoaService,
-            IMapper mapper)
+        public AgendamedicamentoController(IAgendamedicamentoService agendamedicamentoService, IMapper mapper)
         {
             _agendamedicamentoService = agendamedicamentoService;
-            _medicamentoService = medicamentoService;
-            _animalService = animalService;
-            _consultaService = consultaService;
-            //_pessoaService = pessoaService;
             _mapper = mapper;
         }
 
@@ -37,22 +22,14 @@ namespace Controllers
         public ActionResult Index()
         {
             var listaAgendamento = _agendamedicamentoService.ObterTodos();
-            return View(listaAgendamento);
+            var agendamedicamentoModel = _mapper.Map<List<AgendamedicamentoModel>>(listaAgendamento);
+            return View(agendamedicamentoModel);
         }
 
         // GET: AgendamedicamentoController/Details/5
         public ActionResult Details(int id)
         {
             Agendamedicamento agendamedicamento = _agendamedicamentoService.Obter(id);
-            Medicamento medicamento = _medicamentoService.Obter(agendamedicamento.IdMedicamento);
-            Animal animal = _animalService.Obter(agendamedicamento.IdAnimal);
-            Consulta consulta = _consultaService.Obter(agendamedicamento.IdConsulta);
-            ViewBag.Medicamento = medicamento.Nome;
-            ViewBag.Animal = animal.Nome;
-            ViewBag.Consulta = consulta.Descricao;
-            //Pessoa service não está implementada então não será póssível obter o nome diretamente
-            //Pessoa pessoa = _pessoaService.Obter(agendamedicamento.IdPessoa);
-            //ViewBag.Pessoa = pessoa.Nome;
             AgendamedicamentoModel agendamedicamentoModel = _mapper.Map<AgendamedicamentoModel>(agendamedicamento);
             return View(agendamedicamentoModel);
         }
@@ -60,12 +37,6 @@ namespace Controllers
         // GET: AgendamedicamentoController/Create
         public ActionResult Create()
         {
-            IEnumerable<Animal> listaAnimais = _animalService.ObterTodos();
-            IEnumerable<MedicamentoDTO> listaMedicamentos = _medicamentoService.ObterTodos();
-            IEnumerable<Consulta> listaConsultas = _consultaService.ObterTodos();
-            ViewBag.Animais = new SelectList(listaAnimais, "IdAnimal", "Nome", null);
-            ViewBag.Medicamentos = new SelectList(listaMedicamentos, "IdMedicamento", "Nome", null);
-            ViewBag.Consultas = new SelectList(listaConsultas, "IdConsulta", "Nome", null);
             return View();
         }
 
@@ -85,14 +56,8 @@ namespace Controllers
         // GET: AgendamedicamentoController/Edit/5
         public ActionResult Edit(int id)
         {
-            IEnumerable<Animal> listaAnimais = _animalService.ObterTodos();
-            IEnumerable<MedicamentoDTO> listaMedicamentos = _medicamentoService.ObterTodos();
-            IEnumerable<Consulta> listaConsultas = _consultaService.ObterTodos();
             Agendamedicamento agendamedicamento = _agendamedicamentoService.Obter(id);
             AgendamedicamentoModel agendamedicamentoModel = _mapper.Map<AgendamedicamentoModel>(agendamedicamento);
-            ViewBag.Animais = new SelectList(listaAnimais, "IdAnimal", "Nome", null);
-            ViewBag.Medicamentos = new SelectList(listaMedicamentos, "IdMedicamento", "Nome", null);
-            ViewBag.Consultas = new SelectList(listaConsultas, "IdConsulta", "Nome", null);
             return View(agendamedicamentoModel);
         }
 
@@ -113,15 +78,6 @@ namespace Controllers
         public ActionResult Delete(int id)
         {
             Agendamedicamento agendamedicamento = _agendamedicamentoService.Obter(id);
-            Medicamento medicamento = _medicamentoService.Obter(agendamedicamento.IdMedicamento);
-            Animal animal = _animalService.Obter(agendamedicamento.IdAnimal);
-            Consulta consulta = _consultaService.Obter(agendamedicamento.IdConsulta);
-            ViewBag.Medicamento = medicamento.Nome;
-            ViewBag.Animal = animal.Nome;
-            ViewBag.Consulta = consulta.Descricao;
-            //Pessoa service não está implementada então não será póssível obter o nome diretamente
-            //Pessoa pessoa = _pessoaService.Obter(aplicaMedicamento.IdPessoa);
-            //ViewBag.Pessoa = pessoa.Nome;
             AgendamedicamentoModel agendamedicamentoModel = _mapper.Map<AgendamedicamentoModel>(agendamedicamento);
             return View(agendamedicamentoModel);
         }
