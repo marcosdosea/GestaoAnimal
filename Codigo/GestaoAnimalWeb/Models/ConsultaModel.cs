a877f1112eb0ffa833dd9dfd58389c2eb65722b6
valeria_sb@outlook.com.br <06092018lys>
2021-02-01 11:53:07 -0300

    #74 Remoção dos atributos IdConsulta nas views create e edit Adição de mensagens de erro na classe ConsultaModel

--- Codigo/GestaoAnimalWeb/Models/ConsultaModel.cs
+++ Codigo/GestaoAnimalWeb/Models/ConsultaModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Core;
@@ -9,11 +10,26 @@ namespace GestaoAnimalWeb.Models
     public class ConsultaModel
     {
         public int IdConsulta { get; set; }
+
+        [Display(Name = "Descrição da consulta")]
+        [Required(ErrorMessage = "Descrição não pode estar vazia!")]
         public string Descricao { get; set; }
+        [Display(Name = "Horário da Consulta")]
         public TimeSpan Horario { get; set; }
+
+        [Display(Name = "Data da Consulta")]
+        [DataType(DataType.DateTime, ErrorMessage = "Data válida requerida.")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
         public DateTime Data { get; set; }
+
+        [Range(0, 1000,
+        ErrorMessage = "O Valor de uma consulta varia de $0 (grátis) até $1000")]
         public double Preco { get; set; }
+        [Display(Name = "Animal")]
+        [Required(ErrorMessage = "Animal não pode estar vazio.")]
         public int IdAnimal { get; set; }
+        [Display(Name = "Dono")]
+        [Required(ErrorMessage = "Dono não pode estar vazio.")]
         public int IdPessoa { get; set; }
 
         public virtual Animal IdAnimalNavigation { get; set; }
