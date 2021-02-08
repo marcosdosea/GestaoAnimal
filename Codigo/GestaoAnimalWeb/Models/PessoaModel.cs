0a7007d5434a8e0755f4257920d3338e3c0440f5
Thiago Menezes <thiagovasmenezes@gmail.com>
2021-02-08 15:46:30 -0300

    #83 Retirada dos atributos desnecessários de PesspaModel, colocado o link para o index do caso de uso

--- Codigo/GestaoAnimalWeb/Models/PessoaModel.cs
+++ Codigo/GestaoAnimalWeb/Models/PessoaModel.cs
@@ -1,6 +1,8 @@
 ﻿using Core;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,24 +10,39 @@ namespace Models
 {
     public class PessoaModel
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int IdPessoa { get; set; }
+
+        [Display(Name = "Nome")]
+        [Required (ErrorMessage = "Nome não pode estar vazio")]
         public string Nome { get; set; }
+
+        [Display(Name = "Email")]
+        [Required(ErrorMessage = "Nome não pode estar vazio")]
         public string Email { get; set; }
+
+        [Display(Name = "Senha")]
+        [Required(ErrorMessage = "Nome não pode estar vazio")]
         public string Senha { get; set; }
         public string Endereco { get; set; }
         public string Telefone { get; set; }
+
+        [Display(Name = "CPF")]
+        [Required(ErrorMessage = "Nome não pode estar vazio")]
         public string Cpf { get; set; }
+
+        [Display(Name = "Data de Nascimento")]
+        [DataType(DataType.Date, ErrorMessage = "Data válida requerida.")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
         public DateTime? DataNascimento { get; set; }
+
+        [Display (Name ="Sexo")]
+        [Required (ErrorMessage = "Sexo não pode estar ")]
         public string Sexo { get; set; }
         public byte? Ativo { get; set; }
         public string TipoPessoa { get; set; }
         public string Cnpj { get; set; }
         public string Crmv { get; set; }
-
-        public virtual ICollection<Agendamedicamento> Agendamedicamento { get; set; }
-        public virtual ICollection<Animal> Animal { get; set; }
-        public virtual ICollection<Aplicamedicamento> Aplicamedicamento { get; set; }
-        public virtual ICollection<Consulta> Consulta { get; set; }
-        public virtual ICollection<Pessoaorganizacao> Pessoaorganizacao { get; set; }
     }
 }
